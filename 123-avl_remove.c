747f396fb32ae02f9af587f04e6a956fa60b1247
stacie-kipruto <staciekipruto@gmail.com>
2022-11-25 07:46:35 -0800

    AVL -Remove

---
+++ 123-avl_remove.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * avl_remove - remove node from AVL tree
+ * @root: pointer to root node of the tree
+ * @value: value to remove from tree
+ * Return: pointer to new root node of tree after removing value and
+ * after rebalancing
+ */
+
+avl_t *avl_remove(avl_t *root, int value)
+{
+	printf("%d", root->n);
+	printf("%d", value);
+	return (NULL);
+}
